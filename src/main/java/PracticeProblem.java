88fcf149300026dffa43194927bba63825227600
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-04-04 23:53:10 +0000

    Update Practice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,116 @@
+import java.io.*;
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 		
+	}
+    public static String getName(int Line, String file){
+		String name="";
+		Scanner s = null;
+		try{
+			
+			s = new Scanner(new BufferedReader(new FileReader(file)));
+		int counter = 1;
+		while (s.hasNext() && counter < Line){
+			s.nextLine();
+			counter++;
+		}
+
+			if (counter == Line && s.hasNext()){
+				name = s.next() + " " + s.next();
+
+
+			}
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally {
+			if (s != null) {
+				s.close();
+			}
+		}
+		return name;
+		
+	}
+    public static int getAge(int Line, String file){
+		int age =-1;
+		Scanner s = null;
+		try{
+			
+			s = new Scanner(new BufferedReader(new FileReader(file)));
+		int counter = 1;
+		while (s.hasNext() && counter < Line){
+			s.nextLine();
+			counter++;
+		}
+
+			if (counter == Line && s.hasNext()){
+				s.next();
+				s.next();
+				age = s.nextInt();
+			}	
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally {
+			if (s != null) {
+				s.close();
+			}
+		}
+		return age;
+		
+	}
+	public static int getNumber(int Line, String file){
+		int number =-1;
+		Scanner s = null;
+		try{
+			
+			s = new Scanner(new BufferedReader(new FileReader(file)));
+		int counter = 1;
+		while (s.hasNext() && counter < Line){
+			s.nextLine();
+			counter++;
+		}
+
+			if (counter == Line && s.hasNext()){
+				s.next();
+				s.next();
+				s.next();
+				number= s.nextInt();
+			}	
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally {
+			if (s != null) {
+				s.close();
+			}
+		}
+		return number;
+		
+	}
+	public static void fileAppend(String output, String filename){
+		BufferedWriter fw = null;
+		try{
+			fw = new BufferedWriter(new FileWriter(filename, true));
+			fw.write(output);
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally{
+			try{
+				if (fw!=null){
+					fw.close();
+				}
+			}
+			catch(IOException e){
+				System.out.println(e);
+			}
+		}
 	}
 }
